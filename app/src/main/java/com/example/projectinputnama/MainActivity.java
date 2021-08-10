dd9bcd871e8711cb15d8bb4e7a6fad61f4307072
Project 1 Input Nama <ehannuraisah.a4@gmail.com>
2021-08-10 17:20:06 +0700

    penambahan fungsi

--- app/src/main/java/com/example/projectinputnama/MainActivity.java
+++ app/src/main/java/com/example/projectinputnama/MainActivity.java
@@ -3,15 +3,31 @@ package com.example.projectinputnama;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //deklarasi class
+    EditText TxtNama;
+    TextView hasil;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+      //panggil variable
+      TxtNama = (EditText)  findViewById(R.id.txtNama);
+      hasil = (EditText) findViewById(R.id.textMasukkanNamaAnda);
+
+      public void TampilNama (View v){
+          hasil.setText("Nama Anda: "TxtNama.getText());
+        }
 
 
     }
+
 }
